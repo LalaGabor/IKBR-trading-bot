3f7df0fd069f517bbf413f6ff6727377e1176db3
LalaGabor <44199231+LalaGabor@users.noreply.github.com>
2024-04-18 20:37:14 +0200

    Update technical_analysis_manager_tests.py
    Documenting plan for unit tests

--- testing/unit_testing_dir/technical_analysis_manager_tests.py
+++ testing/unit_testing_dir/technical_analysis_manager_tests.py
@@ -1,3 +1,20 @@
+import pytest
+from unittest.mock import MagicMock
+from testing.sample_data_manager  import big_sample_dataframe
+
+#Use a factory to pass sample data to a mocked ta manager object
+@pytest.fixture  # Use decorator function to turn following function definition into an pytest object
+def rsi_calculator_factory():
+    def _rsi_calculator(sample_data):
+        ##TODO: a mocked bot object is not required, as a it is not required on init? How to pass the data then?
+        bot_mock = MagicMock()  # create magic mock object
+        symbol = 'symbol'  # Create dummy symbol string
+        bot_mock.df_dict = {symbol: sample_data}  # assign dummy data to key inside dictionary
+        # Learning, how are we able to access RSICalculator with the below
+        # Answer: the __init__.py file exposes the RSICalculator class constructor when one imports the technical_analysis_manager_dir parent directory
+        return technical_analysis_manager_dir.RSICalculator(bot_mock)
+        # Create and return a mocked TechnicalAnalysisManager instance
+    return _technical_analysis_manager
 # Def Tests
 """
 Test 1: rsi calculator module: test_append_rsi_to_dataframe.
@@ -5,7 +22,17 @@ Test 1: rsi calculator module: test_append_rsi_to_dataframe.
 
 -- Implementation: ingest large sample dataframe. Assert dataframe['Closes'].type() = float
 
--- Status: Open
+-- Status: In progress
+
+def test_append_rsi_to_dataframe(technical_analysis_manager_factory, big_sample_dataframe):
+  store some sample dataframe as a variable
+  run the mock ta manager using the sample dataframe as its argument
+  assert the variable (sample dataframe) rows rsi == some value
+  
+  technical_analysis_manager = technical_analysis_manager_factory(big_sample_dataframe)
+
+  
+  
 
 Test 2: divergence calculator module: test_get_open_candidate.
 -- Test preparation. Find a row number in the sample big dataframe that fulfills the logical checks in the
